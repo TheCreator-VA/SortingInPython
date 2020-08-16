e04fba9be20141118f7474e0f477d5c1f5f37332
Vansh Arora <47741137+Vansh-Arora@users.noreply.github.com>
2020-08-17 02:19:14 +0530

    Added comments explaining the code.

--- mergeSort.py
+++ mergeSort.py
@@ -1,9 +1,11 @@
 def mergeSort(arr, n):
+    # From here we divide.
     if n>1:                     # Divide the array in 2 parts until the length of array is 1
         arr1 = arr[:int(n/2)]
         arr2 = arr[int(n/2):]
         mergeSort(arr1,len(arr1))
         mergeSort(arr2,len(arr2))
+        # In this pasrt we start conquer.
         i=0
         k=0
         j=0
@@ -16,6 +18,10 @@ def mergeSort(arr, n):
                 arr[k] = arr2[j]
                 k+=1
                 j+=1
+        # This part will make sure that if all elements of array 1 or 2 are inserted but some
+        # elements are left in another array.
+        # They are inserted in the same order as present in the remainig array.
+        # As both arrays are already sorted.
         while i<len(arr1):
             arr[k]=arr1[i]
             k+=1
